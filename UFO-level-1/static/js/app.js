d93c7bbc0536415ace5b7e5a3a5503350ebdfd99
KatherineH-cls <katherine.howison@bigpond.com>
2021-09-15 08:59:00 +0800

    loaded data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Add data from each element in data across a row
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
